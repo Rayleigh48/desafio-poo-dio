2e89d5194274a304e61c1584cc67d702b1e5522d
Rayleigh48 <geanfreitas48@gmail.com>
2024-04-22 01:20:52 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+
+import br.com.dio.desafio.Bootcamp;
 import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Dev;
 import br.com.dio.desafio.Mentoria;
+
+
 
 import java.time.LocalDate;
 
@@ -7,19 +12,37 @@ public class Main {
     public static void main(String[] args) {
 
         Curso curso1 = new Curso();
-        Mentoria mentoria = new Mentoria();
+
 
         curso1.setTitulo("curso java");
         curso1.setDescricao("curso de java da prof camila");
-        curso1.setCargaHoraria(9);
+        curso1.setcargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
 
         mentoria.setTitulo("Mentoria");
         mentoria.setDescricao("curso de java");
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(mentoria);
+       // System.out.println(curso1);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("Descrição bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGean = new Dev();
+        devGean.setNome("Gean");
+        devGean.inscreverBootcamp(bootcamp);
+        System.out.println(devGean.getConteudosInscritos());
+        devGean.progredir();
+
+        System.out.println(devGean.getConteudosInscritos());
+        System.out.println(devGean.getConteudosConcluidos());
+        System.out.println("XP" + devGean.calcularTotalXp());
 
     }
 
--- src/br/com/dio/desafio/Mentoria.java
+++ src/br/com/dio/desafio/Mentoria.java
@@ -3,29 +3,20 @@ package br.com.dio.desafio;
 import java.time.LocalDate;
 import java.util.Locale;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
+
     private LocalDate data;
+
+    @Override
+    public double calcularXp(){
+        return XP_PADRAO + 20d;
+    }
 
     public Mentoria() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -39,9 +30,11 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
+
+
 }
